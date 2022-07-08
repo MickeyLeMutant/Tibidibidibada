0ee7bb1540670a72525cf5fa0526a7a2eeadca89
MickeyLeMutant <mehdi.tahan.stock@gmail.com>
2022-07-08 05:24:42 +0200

    Add about

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const join = require('path').join;
+
+
+const openAboutWindow = require('about-window').default;
 
 let mainWindow;
 
-function createWindow () {
+const nativeImage = require('electron').nativeImage;
+var image = nativeImage.createFromPath(join(__dirname, 'img/icon3D.png'));
+
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
-  }
+      contextIsolation: false
+    },
+    icon: image
   });
+  mainWindow.title = 'Tibidibidibada ' + app.getVersion();
   mainWindow.loadFile('index.html');
   mainWindow.on('closed', function () {
     mainWindow = null;
@@ -21,8 +30,34 @@ function createWindow () {
   });
 }
 
+function createMenu() {
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'About This App',
+          click: () =>
+            openAboutWindow({
+              icon_path: join(__dirname, 'img/icon3D.png'),
+              copyright: 'Copyright (c) 2022 Mehdi TAHAN',
+              package_json_dir: __dirname,
+              open_devtools: process.env.NODE_ENV !== 'production',
+            }),
+        },
+        {
+          role: 'quit',
+        },
+      ],
+    },
+  ]);
+  app.applicationMenu = menu;
+}
+
 app.on('ready', () => {
   createWindow();
+
+  createMenu();
 });
 
 app.on('window-all-closed', function () {
