d4c31b10f39bf2bec17f0d369b6253482403e3e3
MickeyLeMutant <mehdi.tahan.stock@gmail.com>
2022-07-09 17:17:39 +0200

    1.0.3 add searchbar and filters

--- main.js
+++ main.js
@@ -1,12 +1,53 @@
-const { app, BrowserWindow, ipcMain, Menu } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const join = require('path').join;
 
 
 const openAboutWindow = require('about-window').default;
 
+const openLibrary = () => {
+  // open dialog
+  dialog.showOpenDialog(mainWindow, { properties: ['openDirectory'] })
+    .then(result => {
+      if (!result.canceled) {
+        const directoryPath = result.filePaths;
+        console.log(directoryPath);
+
+        // load config
+        const fs = require('fs');
+        const cfgPath = directoryPath + `/tibi.cfg`
+        if (fs.existsSync(cfgPath)) {
+          fs.readFile(cfgPath, (err, data) => {
+            if (err) throw err;
+            let cfg = JSON.parse(data);
+            mainWindow.webContents.send('filters_init',{cfg: cfg});
+          });
+        } else {
+          console.log('does not')
+        }
+
+        /*let student = { 
+            name: 'Mike',
+            age: 23, 
+            gender: 'Male',
+            department: 'English',
+            car: 'Honda' 
+        };*/
+
+        //fs.writeFileSync(path.resolve(__dirname, 'student.json'), JSON.stringify(student));
+        // load filters in UI
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+
+
+  console.log("test");
+}
+
 let mainWindow;
 
+// #region UI init
 const nativeImage = require('electron').nativeImage;
 var image = nativeImage.createFromPath(join(__dirname, 'img/icon3D.png'));
 
@@ -31,25 +72,45 @@ function createWindow() {
 }
 
 function createMenu() {
-  const menu = Menu.buildFromTemplate([
-    {
-      label: 'Help',
-      submenu: [
-        {
-          label: 'About This App',
-          click: () =>
-            openAboutWindow({
-              icon_path: join(__dirname, 'img/icon3D.png'),
-              copyright: 'Copyright (c) 2022 Mehdi TAHAN',
-              package_json_dir: __dirname,
-              open_devtools: process.env.NODE_ENV !== 'production',
-            }),
-        },
-        {
-          role: 'quit',
-        },
-      ],
-    },
+  const menu = Menu.buildFromTemplate([{
+    label: 'File',
+    submenu: [
+      {
+        label: 'Open Library',
+        click: () =>
+          openLibrary(),
+      },
+      {
+        label: 'Import in library',
+        click: () =>
+          openAboutWindow({
+            icon_path: join(__dirname, 'img/icon3D.png'),
+            copyright: 'Copyright (c) 2022 Mehdi TAHAN',
+            package_json_dir: __dirname,
+            open_devtools: process.env.NODE_ENV !== 'production',
+          }),
+      },
+    ],
+  },
+  {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'About This App',
+        click: () =>
+          openAboutWindow({
+            icon_path: join(__dirname, 'img/icon3D.png'),
+            copyright: 'Copyright (c) 2022 Mehdi TAHAN',
+            package_json_dir: __dirname,
+            open_devtools: process.env.NODE_ENV !== 'production',
+          }),
+      },
+      {
+        role: 'quit',
+      },
+    ],
+  },
+
   ]);
   app.applicationMenu = menu;
 }
@@ -60,15 +121,17 @@ app.on('ready', () => {
   createMenu();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
 app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
+  }
+});
+
+// #endregion
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
   }
 });
 
@@ -76,6 +139,7 @@ ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
+// #region auto update
 autoUpdater.on('update-available', () => {
   mainWindow.webContents.send('update_available');
 });
@@ -87,3 +151,5 @@ autoUpdater.on('update-downloaded', () => {
 ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
 });
+
+// #endregion
